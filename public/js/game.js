78f2360ac9c6c7c047ef0162aabb8943cb1b9c54
Mike Slemmer <mslemmer@yahoo-inc.com>
2015-10-27 10:11:21 -0700

    more work

---
+++ public/js/game.js
@@ -0,0 +1,97 @@
+$(function() {
+
+    window.Game = {
+        _mouseOverCountry: null,
+
+        init: function() {
+            Globals.context.clearRect(0,0,2000,2000);
+            Globals.context.lineJoin = "straight";
+            
+
+            // Clear the Hex and Country statics.
+            Player.init(8);
+            Hex.init(); 
+            Country.init();
+            var country = new Country(Hex.get(Math.floor(Math.random() * Hex.count())), Player.randomPlayer());
+
+            for (var i = 0; i < 29; i++) {
+                var countryStart = Math.floor(Math.random() * Country.count());
+                var adjacentHex;
+
+                for (var j = 0; j < Country.count(); j++) {
+                    var country = Country.get((j + countryStart) % Country.count());
+                    if (country.isLake()) {
+                        continue;
+                    }
+                    adjacentHex = country.findAdjacentHex(true);
+                    if (adjacentHex) {
+                        break;
+                    }
+                }
+                if (!adjacentHex) {
+                    Globals.debug("RAN OUT OF SPACE!", i);
+                    break;
+                }
+                var newCountry = new Country(adjacentHex, Player.randomPlayer());
+                if (newCountry.isLake()) {
+                    i--;
+                }
+            }
+
+            Hex.absorbSingles();
+            Country.pruneLakes();
+
+            Country.array().forEach(function(country) {
+                country.setupEdges();
+                country.paint();
+            });
+
+            $(Globals.canvas).mousemove(Game.mouseMove);
+            $(Globals.canvas).mouseleave(Game.mouseLeave);
+
+        },
+
+
+        mouseMove: function(event) {
+
+            var hex = Hex.fromMousePos(event.offsetX, event.offsetY);
+            if (hex) {
+                var country = hex.country();
+                if (country) {
+                    Globals.canvas.style.cursor = 'pointer';
+                } else {
+                    Globals.canvas.style.cursor = 'default';
+                }
+
+                if (country != Game._mouseOverCountry) {
+                    if (Game._mouseOverCountry) {
+                        Game._mouseOverCountry.mouseLeave();
+                    }
+                    Game._mouseOverCountry = country;
+                    if (country) {
+                        Game._mouseOverCountry.mouseEnter();                        
+                    }
+                }
+            } else {
+                if (Game._mouseOverCountry) {
+                    Game._mouseOverCountry.mouseLeave();
+                    Game._mouseOverCountry = null;
+                }
+                Globals.canvas.style.cursor = 'default';
+            }
+            //    document.getElementById("info").textContent = hex.num();
+//                document.getElementById("info").textContent = hex;
+        },
+
+        mouseLeave: function(event) {
+            if (Game._mouseOverCountry) {
+                Game._mouseOverCountry.mouseLeave();
+                Game._mouseOverCountry = null;
+            }
+        }
+
+    }
+});
+
+
+
