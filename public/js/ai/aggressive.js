34365e79dcbd72f5f7926193656857d0ec08eb2b
Mike Slemmer <mslemmer@yahoo-inc.com>
2015-10-30 23:45:47 -0700

    oops

---
+++ public/js/ai/aggressive.js
@@ -0,0 +1,70 @@
+$(function() {
+
+	/*
+		Here is what the interface contains:
+
+		getState() 
+			Returns the game state:
+				{
+					players
+
+
+					countries
+
+
+					currentPlayerId
+						The id of the player who is playing right now.
+
+				}
+       	
+       	attack(fromCountryId, toCountryId) 
+			Called by the AI when it wants to attack a country.
+			Returns a result
+
+        endTurn()
+			Called by the AI when its turn is over
+	*/
+
+	window.AI = window.AI || {};
+	window.AI.Aggressive = {
+		// Called when the AI is first started. Tells the AI its player number
+		// and the list of other players, so it can know who is human and where
+		// in the turn order this AI shows up.
+		init: function(playerId, isHumanList) {
+		},
+
+		// Called each time the AI has a turn.
+		startTurn: function(interface) {
+			var state = interface.getState();
+
+			var playerId = state.currentPlayerId;
+			var countryIds = Object.keys(state.countries);
+			for (var i = 0; i < countryIds.length; i++) {
+				var countryId = countryIds[i];
+				var country = state.countries[countryId];
+				if (country.owner == playerId) {
+					// Somehow it's improperly getting here. Something dumb.
+					var possibleAttacks = [];
+					country.adjacentCountries.forEach(function(adjacentCountryId) {
+						var adjacentCountry = state.countries[adjacentCountryId];
+						if (adjacentCountry.owner != playerId && country.numDice > 1 && 
+							country.numDice >= adjacentCountry.numDice) {
+							possibleAttacks.push(adjacentCountry.id);
+						}
+					});
+
+					if (possibleAttacks.length > 0) {
+						var attackCountryId = possibleAttacks[Math.floor(Math.random() * possibleAttacks.length)];
+						console.log("(aggressive) Attack", countryId, attackCountryId);
+						interface.attack(countryId, attackCountryId);
+						AI.Aggressive.startTurn(interface);	// Continue attacking.
+						return;
+					}
+				}
+			}
+
+			interface.endTurn();
+		}	
+	};
+
+});
