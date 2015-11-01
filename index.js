558b80e71d19d27655c349a76fb00ce63163f9b4
Mike Slemmer <mslemmer@yahoo-inc.com>
2015-11-01 14:14:21 -0800

    minor stuff

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+
 // Turn on handlebars.
 var exphbs = require('express-handlebars');
 app.engine('.hbs', exphbs({defaultLayout: 'single', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-app.use(express.static('public'));
-
 app.get('/', function(req, res) { 
     res.render("index");
 });
 
-app.listen(3000, function() {
-
-
+app.listen(app.get('port'), function() {
+      console.log('Node app is running on port', app.get('port'));
 });
-
 
 
 module.exports = app;
