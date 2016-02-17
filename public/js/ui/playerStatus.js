fb9e449af7983fcf4127442c6252d7aad1b4d07c
Andrew Roper <andrewroper@gmail.com>
2016-02-17 17:16:34 -0600

    missed a file

--- public/js/ui/playerStatus.js
+++ public/js/ui/playerStatus.js
@@ -37,12 +37,16 @@ $(function() {
 			// add a "country count" div for each player
 			for (var id=0; id < playerCount; ++id) {
 				
+				var colorHint = self._players[id] + ' owns all regions of this color.';
+				var contigousHint = 'Number of adjacent regions owned by ' + self._players[id];
+				var storedHint = 'Number of dice ' + self._players[id] + ' has stored';
 				$('#players').append(
-		    		"<div id='player" + id + "' class='col-sm-2 player-box'><div id='colorblock" + id + "' class='color-block'></div>" +
+					"<div id='player" + id + "' class='col-sm-2 player-box'>" + 
+					"<div id='colorblock" + id + "' class='color-block status-icon hint--info' data-hint='" + colorHint + "'></div>" +
 					((self._players && self._players[id]) ? ("<div id='name" + id + "' class='name-box'>" + self._players[id] + "</div>") : "") +
-		    		"<div id='dice" + id + "' class='dice-box'>1</div>" +
-		    		"<div id='stored" + id + "' class='stored-box'>0</div></div>"
-		    	);
+					"<div id='dice" + id + "' class='dice-box status-icon hint--info' data-hint='" + contigousHint + "'>1</div>" +
+					"<div id='stored" + id + "' class='stored-box status-icon hint--info' data-hint='" + storedHint + "'>0</div></div>"
+					);
 
 		    	$('#colorblock' + id).css( 
 			    	{		
